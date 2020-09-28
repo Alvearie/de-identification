2dc27d649bdfbd88158222e2ac1f57364c19ad16
c3cvpfj7@ca.ibm.com <c3cvpfj7@ca.ibm.com>
2020-09-28 14:58:16 -0400

    Added samples

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # de-identification
 The Data De-Identification service provides a wide range of de-identification capabilities designed to support GDPR, HIPAA, CCPA and other privacy frameworks allowing customers to meet their regulatory and privacy requirements.
+
+The samples directory contains the following files:
+- deid_config.json: Sample configuration file used to mask a FHIR JSON
+- deid_curl.sh: Sample curl request to de-identification service
+- deid_input.json:  Sample input to be masked
+- deid_output.json: Sample output
---
+++ samples/deid_curl.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+#  Sample REST API to de-identify a FHIR JSON. The API takes a configuration file and a FHIR JSON as input and
+#  returns de-identified data. 
+#
+#  The expected response:
+#  {
+#    "data": [
+#      {
+#        "resourceType": "Patient",
+#        "id": "example",
+#        "name": [
+#          {
+#            "use": "official",
+#            "family": "NATHO",
+#            "given": [
+#              "EA72C79594296E45B8C2A296644D988581F58CFAC6601D122ED0A8BD7C02E8BF",
+#              "9345A35A6FDF174DFF7219282A3AE4879790DBB785C70F6FFF91E32FAFD66EAB"
+#            ]
+#          },
+#          {
+#            "use": "usual",
+#            "given": [
+#              "96BD923157C731249A40C36426FC326062AD3B2904ED6792B3F404F223D35651"
+#            ]
+#          }
+#        ],
+#        "telecom": [
+#          {
+#            "system": "phone",
+#            "value": "+1-3478057810",
+#            "use": "work",
+#            "rank": 1
+#          }
+#        ],
+#        "birthDate": "1974"
+#      }
+#    ]
+#  }
+#
+
+
+curl -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "config":"{\"rules\":[{\"name\":\"HASH\",\"maskingProviders\":[{\"type\":\"HASH\"}]},{\"name\":\"PHONE\",\"maskingProviders\":[{\"type\":\"PHONE\"}]},{\"name\":\"NAME\",\"maskingProviders\":[{\"type\":\"NAME\"}]},{\"name\":\"MaskBirthDay\",\"maskingProviders\":[{\"type\":\"DATETIME\",\"generalizeYear\":true}]}],\"json\":{\"schemaType\":\"FHIR\",\"messageTypeKey\":\"resourceType\",\"messageTypes\":[\"Patient\"],\"maskingRules\":[{\"jsonPath\":\"/fhir/Patient/name/given\",\"rule\":\"HASH:null\"},{\"jsonPath\":\"/fhir/Patient/name/family\",\"rule\":\"NAME:null\"},{\"jsonPath\":\"/fhir/Patient/telecom/value\",\"rule\":\"PHONE:null\"},{\"jsonPath\":\"/fhir/Patient/birthDate\",\"rule\":\"MaskBirthDay:null\"}]}}" , "data": [  "{\"resourceType\":\"Patient\",\"id\":\"example\",\"name\":[{\"use\":\"official\",\"family\":\"Chalmers\",\"given\":[\"Peter\",\"James\"]},{\"use\":\"usual\",\"given\":[\"Jim\"]}],\"telecom\":[{\"system\":\"phone\",\"value\":\"+1-3471234567\",\"use\":\"work\",\"rank\":1}],\"birthDate\":\"1974-12-25\"}"  ], "schemaType": "FHIR" }' 'https://localhost:8443/api/v1/deidentification'
