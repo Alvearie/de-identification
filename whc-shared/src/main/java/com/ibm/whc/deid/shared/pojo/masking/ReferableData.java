8a90227c012d83f4781edbf374cc51f5da6a9d71
goffr8 <goff1@us.ibm.com>
2020-11-17 12:01:33 -0500

    Use IdentifiedData for messages (#11)
    Use ReferableData for messages

---
+++ whc-shared/src/main/java/com/ibm/whc/deid/shared/pojo/masking/ReferableData.java
@@ -0,0 +1,42 @@
+/*
+ * (C) Copyright IBM Corp. 2016,2020
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.ibm.whc.deid.shared.pojo.masking;
+
+/*
+ * The model class which holds a reference string identifier and some data
+ */
+public class ReferableData {
+
+  private String identifier;
+  private String data;
+
+  public ReferableData(String data) {
+    this.identifier = null;
+    this.data = data;
+  }
+
+  public ReferableData(String identifier, String data) {
+    this.identifier = identifier;
+    this.data = data;
+  }
+
+  public String getIdentifier() {
+    return identifier;
+  }
+
+  public void setIdentifier(String identifier) {
+    this.identifier = identifier;
+  }
+
+  public String getData() {
+    return data;
+  }
+
+  public void setData(String data) {
+    this.data = data;
+  }
+
+}
