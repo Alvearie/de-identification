0bbc7d300fafd40f46ccfac4d5035f6048692f36
c3cvpfj7@ca.ibm.com <c3cvpfj7@ca.ibm.com>
2020-12-10 12:46:27 -0600

    unit tests

--- ipv-core/src/main/java/com/ibm/whc/deid/providers/masking/MaintainMaskingProvider.java
+++ ipv-core/src/main/java/com/ibm/whc/deid/providers/masking/MaintainMaskingProvider.java
@@ -5,10 +5,17 @@
  */
 package com.ibm.whc.deid.providers.masking;
 
+import com.ibm.whc.deid.providers.masking.fhir.MaskingActionInputIdentifier;
+import java.util.List;
+
 public class MaintainMaskingProvider extends AbstractMaskingProvider {
 
-	/** */
-	private static final long serialVersionUID = -1799896601470556794L;
+  private static final long serialVersionUID = -1799896601470556794L;
+
+  @Override
+  public void maskIdentifierBatch(List<MaskingActionInputIdentifier> identifiers) {
+    // override superclass to retain data type of original node
+  }
 
   @Override
   public String mask(String identifier) {
