e70c429a635960a466022d0265e7709a9e99ea62
jarrett@us.ibm.com <jarrett@us.ibm.com>
2021-02-09 18:18:11 -0600

    whd-1046 unspecified value handling for SWIFT

--- ipv-core/src/test/java/com/ibm/whc/deid/providers/masking/SWIFTCodeMaskingProviderTest.java
+++ ipv-core/src/test/java/com/ibm/whc/deid/providers/masking/SWIFTCodeMaskingProviderTest.java
@@ -1,10 +1,12 @@
 /*
- * (C) Copyright IBM Corp. 2016,2020
+ * (C) Copyright IBM Corp. 2016,2021
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.ibm.whc.deid.providers.masking;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import com.ibm.whc.deid.providers.identifiers.Identifier;
@@ -14,20 +16,9 @@ import com.ibm.whc.deid.shared.pojo.config.masking.SWIFTMaskingProviderConfig;
 public class SWIFTCodeMaskingProviderTest extends TestLogSetUp implements MaskingProviderTest {
 
   @Test
-  public void testDefault() {
-    SWIFTMaskingProviderConfig maskingConfiguration = new SWIFTMaskingProviderConfig();
-    MaskingProvider maskingProvider = new SWIFTCodeMaskingProvider(maskingConfiguration, tenantId);
-    Identifier identifier = new SWIFTCodeIdentifier();
-
-    String key = "EMCRGRA1";
-    String maskedValue = maskingProvider.mask(key);
-    assertTrue(identifier.isOfThisType(maskedValue));
-  }
-
-  @Test
   public void testPreserveCountry() {
     SWIFTMaskingProviderConfig maskingConfiguration = new SWIFTMaskingProviderConfig();
-    maskingConfiguration.setPreserveCountry(Boolean.TRUE);
+    maskingConfiguration.setPreserveCountry(true);
     MaskingProvider maskingProvider = new SWIFTCodeMaskingProvider(maskingConfiguration, tenantId);
     Identifier identifier = new SWIFTCodeIdentifier();
 
@@ -38,7 +29,7 @@ public class SWIFTCodeMaskingProviderTest extends TestLogSetUp implements Maskin
 
     System.out.println(maskedValue);
     assertTrue(identifier.isOfThisType(maskedValue));
-    assertTrue(countryCodeOriginal.equals(countryCodeMasked));
+    assertEquals(countryCodeOriginal, countryCodeMasked);
   }
 
   @Test
@@ -62,4 +53,51 @@ public class SWIFTCodeMaskingProviderTest extends TestLogSetUp implements Maskin
     assertTrue(randomizationOK > 0);
   }
 
+  @Test
+  public void testInvalidInput() {
+    SWIFTMaskingProviderConfig maskingConfiguration = new SWIFTMaskingProviderConfig();
+    MaskingProvider maskingProvider = new SWIFTCodeMaskingProvider(maskingConfiguration, tenantId);
+    Identifier identifier = new SWIFTCodeIdentifier();
+
+    assertNull(maskingProvider.mask((String) null));
+    assertNull(maskingProvider.mask(""));
+    assertNull(maskingProvider.mask(" "));
+    assertNull(maskingProvider.mask("invalid"));
+
+    maskingConfiguration.setUnspecifiedValueHandling(2);
+    maskingProvider = new SWIFTCodeMaskingProvider(maskingConfiguration, tenantId);
+
+    assertNull(maskingProvider.mask((String) null));
+    String maskedValue = maskingProvider.mask("");
+    assertTrue(identifier.isOfThisType(maskedValue));
+    maskedValue = maskingProvider.mask(" ");
+    assertTrue(identifier.isOfThisType(maskedValue));
+    maskedValue = maskingProvider.mask("invalid");
+    assertTrue(identifier.isOfThisType(maskedValue));
+
+    maskingConfiguration.setUnspecifiedValueHandling(3);
+    maskingProvider = new SWIFTCodeMaskingProvider(maskingConfiguration, tenantId);
+
+    assertNull(maskingProvider.mask((String) null));
+    assertEquals("OTHER", maskingProvider.mask(""));
+    assertEquals("OTHER", maskingProvider.mask(" "));
+    assertEquals("OTHER", maskingProvider.mask("invalid"));
+
+    maskingConfiguration.setUnspecifiedValueHandling(1);
+    maskingProvider = new SWIFTCodeMaskingProvider(maskingConfiguration, tenantId);
+
+    assertNull(maskingProvider.mask((String) null));
+    assertNull(maskingProvider.mask(""));
+    assertNull(maskingProvider.mask(" "));
+    assertNull(maskingProvider.mask("invalid"));
+
+    maskingConfiguration.setUnspecifiedValueHandling(3);
+    maskingConfiguration.setUnspecifiedValueReturnMessage("msg");
+    maskingProvider = new SWIFTCodeMaskingProvider(maskingConfiguration, tenantId);
+
+    assertNull(maskingProvider.mask((String) null));
+    assertEquals("msg", maskingProvider.mask(""));
+    assertEquals("msg", maskingProvider.mask(" "));
+    assertEquals("msg", maskingProvider.mask("invalid"));
+  }
 }
