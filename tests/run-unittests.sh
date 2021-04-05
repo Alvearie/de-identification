8ca749b89f0366caa27a65315ed01e7679445b85
c3cvpfj7@ca.ibm.com <c3cvpfj7@ca.ibm.com>
2021-04-05 13:54:25 -0400

    Test PR

--- tests/run-unittests.sh
+++ tests/run-unittests.sh
@@ -6,7 +6,6 @@
 # 
 #  Format junit test result for IBM toolchain
 #
-#
 
 
 echo -e '<?xml version="1.0" encoding="UTF-8" ?>\n<testsuites>' > unittest.xml; for i in `find ../ -name surefire-reports`; do for j in `ls $i/*xml`; do cat $j | grep -v '<?xml version' >> unittest.xml; echo >> unittest.xml; done; done; echo '</testsuites>' >> unittest.xml; cat unittest.xml
