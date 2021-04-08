d992efb0dd4d3e4194b60b1bfdd78bf638c14c61
c3cvpfj7@ca.ibm.com <c3cvpfj7@ca.ibm.com>
2021-04-08 10:03:25 -0400

    added comment

--- preDockerBuild.sh
+++ preDockerBuild.sh
@@ -33,6 +33,8 @@ fi
 
 curl -sSL "https://${gitApiKey}@raw.github.ibm.com/de-identification/de-id-devops/${DEVELOPER_BRANCH}/scripts/de-identification-settings.xml" > ${HOME}/.m2/settings.xml
 
+# Set the version.  If the branch is master, use the ${RELEASE_VERSION}-SNAPSHOT
+# If the branch is not master, include branch name in the version
 RELEASE_VERSION=1.0.0
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 if [ "$branch" == "master" ]; then
