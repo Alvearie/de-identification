f306def6694397847752df3e1e732a2044ce5f2f
jarrett@us.ibm.com <jarrett@us.ibm.com>
2021-04-09 11:41:04 -0500

    whd-1028 changes from additional unit tests

--- ipv-core/src/test/java/com/ibm/whc/deid/util/ATCManagerTest.java
+++ ipv-core/src/test/java/com/ibm/whc/deid/util/ATCManagerTest.java
@@ -6,7 +6,9 @@
 package com.ibm.whc.deid.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import com.ibm.whc.deid.util.localization.LocalizationManager;
 
@@ -17,5 +19,7 @@ public class ATCManagerTest {
     ATCManager mgr = ATCManager.buildATCManager(LocalizationManager.DEFAULT_LOCALIZATION_PROPERTIES);
     assertNotNull(mgr);
     assertEquals(3681, mgr.getValues().size());
+    assertTrue(mgr.isValidKey("A01AB19"));    
+    assertFalse(mgr.isValidKey("A01AB19X"));
   } 
 }
