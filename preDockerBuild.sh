a1b31337eaaadfe4937b27af8fb51fe842febae9
jarrett@us.ibm.com <jarrett@us.ibm.com>
2021-10-18 13:01:53 -0500

    whd-1485 syntax corrections
    Signed-off-by: jarrett@us.ibm.com <jarrett@us.ibm.com>

--- preDockerBuild.sh
+++ preDockerBuild.sh
@@ -91,7 +91,7 @@ fi
 #########################################################
 # Deploy the binaries to artifactory using maven        #
 #########################################################
-if [ "${RELEASE_BUILD} == "true" ]; then
+if [ "${RELEASE_BUILD}" == "true" ]; then
     MAVEN_REPO=releases::default::https://na.artifactory.swg-devops.com:443/artifactory/wh-de-id-release-maven-local
 else    
     MAVEN_REPO=snapshots::default::https://na.artifactory.swg-devops.com:443/artifactory/wh-de-id-snapshot-maven-local
