7e5277b1f2c5217940d959bf5946a3324fd5d10a
jarrett@us.ibm.com <jarrett@us.ibm.com>
2021-12-14 15:16:34 -0600

    whd-1606 remove log4j
    Signed-off-by: jarrett@us.ibm.com <jarrett@us.ibm.com>

--- ipv-core/src/test/java/com/ibm/whc/deid/providers/masking/TestLogSetUp.java
+++ ipv-core/src/test/java/com/ibm/whc/deid/providers/masking/TestLogSetUp.java
@@ -1,53 +1,64 @@
 /*
- * (C) Copyright IBM Corp. 2016,2020
+ * (C) Copyright IBM Corp. 2016,2021
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.ibm.whc.deid.providers.masking;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ibm.whc.deid.utils.log.LogManager;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import org.apache.log4j.Appender;
-import org.apache.log4j.Layout;
-import org.apache.log4j.Logger;
-import org.apache.log4j.SimpleLayout;
-import org.apache.log4j.WriterAppender;
 import org.junit.After;
 import org.junit.Before;
+import org.slf4j.LoggerFactory;
+import com.ibm.whc.deid.utils.log.LogManager;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.CoreConstants;
+import ch.qos.logback.core.OutputStreamAppender;
+import ch.qos.logback.core.encoder.EchoEncoder;
+
 
 public class TestLogSetUp {
 
-  private static final String APPENDER_NAME = "log4jRuleAppender";
-  private static final Layout LAYOUT = new SimpleLayout();
+  /*
+   * Use slf4j to get a logger instance. Use the same class as was used by LogManager. The same
+   * concrete object should be returned. The application uses slf4j to get access to logging, either
+   * directly or through the LogManager class. The actual logger object returned will be based on
+   * logback, per the application's logging configuration. Cast it appropriately so logging events
+   * can be intercepted for testing.
+   */
+  private static final Logger appLogger = (Logger) LoggerFactory.getLogger(LogManager.class);
 
-  public Logger logger;
-  public ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  // accessed by subclasses to check log records
+  public final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-  protected ObjectMapper objectMapper = new ObjectMapper();
+  private final OutputStreamAppender<ILoggingEvent> appender = new OutputStreamAppender<>();
+  private final EchoEncoder<ILoggingEvent> encoder = new EchoEncoder<ILoggingEvent>() {
+    @Override
+    public byte[] encode(ILoggingEvent event) {
+      // match the output to the layout returned by the previous
+      // version of test utility that used log4j 1.2 returned
+      StringBuilder sb = new StringBuilder();
+      sb.append(event.getLevel()).append(" - ");
+      sb.append(event.getFormattedMessage());
+      sb.append(CoreConstants.LINE_SEPARATOR);
+      return sb.toString().getBytes();
+    }
+  };
+
+  // protected ObjectMapper objectMapper = new ObjectMapper();
+
 
   @Before
   public void setupAppender() {
-    logger = Logger.getLogger(LogManager.class);
-    Appender appender = new WriterAppender(LAYOUT, outContent);
-    appender.setName(APPENDER_NAME);
-    logger.addAppender(appender);
+    appender.setEncoder(encoder);
+    appender.setOutputStream(outContent);
+    appender.start();
+    appLogger.addAppender(appender);
   }
 
   @After
-  public void tearAppender() throws IOException {
-    logger.removeAllAppenders();
-    outContent.flush();
-    outContent.close();
-  }
-
-  protected void printJsonObject(Object json) {
-    try {
-      System.out.println(objectMapper.writeValueAsString(json));
-    } catch (JsonProcessingException e) {
-      // ignore exception
-    }
+  public void tearAppender() {
+    appLogger.detachAppender(appender);
+    appender.stop();
   }
 }
