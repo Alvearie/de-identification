84eb7b4bc5fc8bd494a28e74e68537663e394758
jarrettranderson <74270100+jarrettranderson@users.noreply.github.com>
2021-12-15 12:16:59 -0600

    Remove log4j (#104)

--- ipv-utils/src/test/java/com/ibm/whc/deid/utils/log/LogManagerTest.java
+++ ipv-utils/src/test/java/com/ibm/whc/deid/utils/log/LogManagerTest.java
@@ -1,91 +1,125 @@
 /*
- * (C) Copyright IBM Corp. 2016,2020
+ * (C) Copyright IBM Corp. 2016,2021
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 package com.ibm.whc.deid.utils.log;
 
-import static org.hamcrest.CoreMatchers.anyOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import org.apache.log4j.Appender;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggingEvent;
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 
-@RunWith(MockitoJUnitRunner.class)
 public class LogManagerTest {
 
-  @Mock
-  Appender mockAppender;
+  private static final LogManager logManager = LogManager.getInstance();
 
-  @Captor
-  private ArgumentCaptor<LoggingEvent> captorLoggingEvent;
+  /*
+   * Use slf4j to get a logger instance. Use the same class as was used by LogManager. The same
+   * concrete object should be returned. The application uses slf4j to get access to logging, either
+   * directly or through the LogManager class. The actual logger object returned will be based on
+   * logback, per the application's logging configuration. Cast it appropriately so logging events
+   * can be intercepted for testing.
+   */
+  private static final Logger appLogger = (Logger) LoggerFactory.getLogger(LogManager.class);
 
-  private static LogManager log = LogManager.getInstance();
-
-  @Before
-  public void setup() {
-    Logger logger = Logger.getRootLogger();
-    when(mockAppender.getName()).thenReturn("MOCK");
-    // when(mockAppender.isStarted()).thenReturn(true);
-    logger.addAppender(mockAppender);
-  }
 
   @Test
   public void testLogInfo() {
-    log.logInfo(LogCodes.WPH1000I, "This is test message");
+    ListAppender<ILoggingEvent> appender = new ListAppender<>();
+    appender.start();
+    appLogger.addAppender(appender);
 
-    verify(mockAppender).doAppend(captorLoggingEvent.capture());
-    final LoggingEvent loggingEvent = captorLoggingEvent.getValue();
-    assertThat(loggingEvent.getLevel(), is(Level.INFO));
-    assertThat(loggingEvent.getMessage(), anyOf(is(
-        "WPH1000I - com.ibm.whc.deid.utils.log.LogManager - logInfo - IPV-core information : \"This is test message\""),
-        is("WPH1000I - com.ibm.whc.deid.utils.log.LogManagerTest - testLogInfo - IPV-core information : \"This is test message\"")));
+    try {
+      logManager.logInfo(LogCodes.WPH1000I, "This is test message");
+
+      assertEquals(1, appender.list.size());
+      ILoggingEvent event = appender.list.get(0);
+      // note - current implementation gets source class and method for the message from the stack
+      String msg = event.getFormattedMessage();
+      assertEquals(
+          "WPH1000I - com.ibm.whc.deid.utils.log.LogManagerTest - testLogInfo - IPV-core information : \"This is test message\"",
+          msg);
+      assertEquals(Level.INFO, event.getLevel());
+      System.out.println(msg);
+    } finally {
+      appLogger.detachAppender(appender);
+      appender.stop();
+    }
   }
 
   @Test
   public void testLogWarn() {
-    log.logWarn(LogCodes.WPH1012W, "This is test message");
+    ListAppender<ILoggingEvent> appender = new ListAppender<>();
+    appender.start();
+    appLogger.addAppender(appender);
 
-    verify(mockAppender).doAppend(captorLoggingEvent.capture());
-    final LoggingEvent loggingEvent = captorLoggingEvent.getValue();
-    assertThat(loggingEvent.getLevel(), is(Level.WARN));
-    assertThat(loggingEvent.getMessage(), anyOf(is(
-        "WPH1012W - com.ibm.whc.deid.utils.log.LogManager - logWarn - IPV-core warning : \"This is test message\""),
-        is("WPH1012W - com.ibm.whc.deid.utils.log.LogManagerTest - testLogWarn - IPV-core warning : \"This is test message\"")));
+    try {
+      logManager.logWarn(LogCodes.WPH1012W, "This is test message");
+
+      assertEquals(1, appender.list.size());
+      ILoggingEvent event = appender.list.get(0);
+      // note - current implementation gets source class and method for the message from the stack
+      String msg = event.getFormattedMessage();
+      assertEquals(
+          "WPH1012W - com.ibm.whc.deid.utils.log.LogManagerTest - testLogWarn - IPV-core warning : \"This is test message\"",
+          msg);
+      assertEquals(Level.WARN, event.getLevel());
+      System.out.println(msg);
+    } finally {
+      appLogger.detachAppender(appender);
+      appender.stop();
+    }
   }
 
   @Test
   public void testLogError() {
-    log.logError(LogCodes.WPH1013E, "This is test message");
+    ListAppender<ILoggingEvent> appender = new ListAppender<>();
+    appender.start();
+    appLogger.addAppender(appender);
 
-    verify(mockAppender).doAppend(captorLoggingEvent.capture());
-    final LoggingEvent loggingEvent = captorLoggingEvent.getValue();
-    assertThat(loggingEvent.getLevel(), is(Level.ERROR));
-    assertThat(loggingEvent.getMessage(), anyOf(is(
-        "WPH1013E - com.ibm.whc.deid.utils.log.LogManager - logError - IPV-core error : \"This is test message\""),
-        is("WPH1013E - com.ibm.whc.deid.utils.log.LogManagerTest - testLogError - IPV-core error : \"This is test message\"")));
+    try {
+      logManager.logError(LogCodes.WPH1013E, "This is test message");
+
+      assertEquals(1, appender.list.size());
+      ILoggingEvent event = appender.list.get(0);
+      // note - current implementation gets source class and method for the message from the stack
+      String msg = event.getFormattedMessage();
+      assertEquals(
+          "WPH1013E - com.ibm.whc.deid.utils.log.LogManagerTest - testLogError - IPV-core error : \"This is test message\"",
+          msg);
+      assertEquals(Level.ERROR, event.getLevel());
+      System.out.println(msg);
+    } finally {
+      appLogger.detachAppender(appender);
+      appender.stop();
+    }
   }
 
   @Test
   public void testLogDebug() {
-    log.logDebug(LogCodes.WPH1015D, "This is test message");
+    ListAppender<ILoggingEvent> appender = new ListAppender<>();
+    appender.start();
+    appLogger.addAppender(appender);
 
-    verify(mockAppender).doAppend(captorLoggingEvent.capture());
-    final LoggingEvent loggingEvent = captorLoggingEvent.getValue();
-    assertThat(loggingEvent.getLevel(), is(Level.DEBUG));
-    assertThat(loggingEvent.getMessage(), anyOf(is(
-        "WPH1015D - com.ibm.whc.deid.utils.log.LogManager - logError - IPV-core error : \"This is test message\""),
-        is("WPH1015D - com.ibm.whc.deid.utils.log.LogManagerTest - testLogDebug - The parameter \"This is test message\" processed by class \"{1}\" is not found or invalid")));
+    try {
+      logManager.logDebug(LogCodes.WPH1015D, "This is test message");
+
+      assertEquals(1, appender.list.size());
+      ILoggingEvent event = appender.list.get(0);
+      // note - current implementation gets source class and method for the message from the stack
+      String msg = event.getFormattedMessage();
+      assertEquals(
+          "WPH1015D - com.ibm.whc.deid.utils.log.LogManagerTest - testLogDebug - The parameter \"This is test message\" processed by class \"{1}\" is not found or invalid",
+          msg);
+      assertEquals(Level.DEBUG, event.getLevel());
+      System.out.println(msg);
+    } finally {
+      appLogger.detachAppender(appender);
+      appender.stop();
+    }
   }
 }
