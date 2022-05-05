8b1c7497fa00f0e8203161eca1d03ca75780e815
jarrett@us.ibm.com <jarrett@us.ibm.com>
2022-05-05 14:24:26 -0500

    whd-1781 disable unused functions
    Signed-off-by: jarrett@us.ibm.com <jarrett@us.ibm.com>

--- whc-shared/src/main/java/com/ibm/whc/deid/shared/pojo/config/ConfigSchemaTypes.java
+++ whc-shared/src/main/java/com/ibm/whc/deid/shared/pojo/config/ConfigSchemaTypes.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright IBM Corp. 2016,2020
+ * (C) Copyright IBM Corp. 2016,2022
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 /*
  * Interface for ConfigSchemaTypes - must use.
  */
-@Schema(name = "configSchemaTypes", description = "Type of config schema")
+@Schema(name = "configSchemaTypes", description = "input data format")
 public interface ConfigSchemaTypes {
   public String name();
 }
--- whc-shared/src/main/java/com/ibm/whc/deid/shared/pojo/masking/DataMaskingModel.java
+++ whc-shared/src/main/java/com/ibm/whc/deid/shared/pojo/masking/DataMaskingModel.java
@@ -20,24 +20,28 @@ public class DataMaskingModel {
   @Schema(description = "Masking configuration")
   protected final String config;
 
-  @Schema(description = "Document level processing configuration")
-  protected final String globalConfig;
+  // @Schema(description = "Document level processing configuration")
+  // protected final String globalConfig;
 
   @Schema(description = "A list of data to be masked")
   protected final List<String> data;
 
-  @Schema(description = "The format of the data to be masked")
+  @Schema(description = "The format of the data to be masked", implementation = String.class)
   protected final ConfigSchemaTypes schemaType;
 
+  // remove this JsonCreator if constructor with globalConfig is reinstated
+  @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
   public DataMaskingModel(@JsonProperty("config") String config,
       @JsonProperty("data") List<String> data,
       @JsonProperty("schemaType") ConfigSchemaTypes schemaType) {
     this.config = config;
     this.data = data;
     this.schemaType = schemaType;
-    this.globalConfig = null;
+    // this.globalConfig = null;
   }
 
+  //@formatter:off
+  /*
   @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
   public DataMaskingModel(@JsonProperty("config") String config,
       @JsonProperty("globalConfig") String globalConfig,
@@ -48,14 +52,20 @@ public class DataMaskingModel {
     this.schemaType = schemaType;
     this.globalConfig = globalConfig;
   }
+  */
+  //@formatter:on
 
   public String getConfig() {
     return config;
   }
 
+  //@formatter:off
+  /*
   public String getGlobalConfig() {
     return globalConfig;
   }
+  */
+  //@formatter:on
 
   public List<String> getData() {
     return data;
